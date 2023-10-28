0d7b827d190962d737c8aa959d15799b021e5fe5
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 12:31:12 +0330

    Add contractor calss

---
+++ WasteManagementAPI/Models/Contractor.cs
@@ -0,0 +1,12 @@
+﻿namespace WasteManagementAPI.Models
+{
+
+    public class Contractor
+    {
+        public int ContractorId { get; set; }
+
+        public string Nmae { get; set; }
+
+        public string Family { get; set; }
+    }
+}
