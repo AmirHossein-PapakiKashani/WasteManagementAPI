400efb1af6c73b2fbb672338be2633261adf7af8
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 12:33:35 +0330

    Add Employee class for mange them

---
+++ WasteManagementAPI/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WasteManagementAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Family { get; set; } = string.Empty;
+    }
+}
