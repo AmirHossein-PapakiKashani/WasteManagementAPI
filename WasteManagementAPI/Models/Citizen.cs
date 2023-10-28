033aa3f54ac4aee6402c92f6dd4de5465280a06a
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 12:09:55 +0330

    Add citizen model for manage them

---
+++ WasteManagementAPI/Models/Citizen.cs
@@ -0,0 +1,14 @@
+﻿namespace WasteManagementAPI.Models
+{
+    public class Citizen
+    {
+          
+        public int CitizenId { get; set; }
+        
+        public string Name { get; set; } = string.Empty;
+
+        public string Family { get; set; } = string.Empty;
+
+        public int? Score { get; set; }
+    }
+}
