fafc6e4db59cf3180993decee9a03a0517495363
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 12:17:34 +0330

    Add Product class for manage products

---
+++ WasteManagementAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WasteManagementAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Weight { get; set; }
+
+
+    }
+}
