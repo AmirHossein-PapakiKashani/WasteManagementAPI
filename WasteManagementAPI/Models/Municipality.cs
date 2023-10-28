d4501941be5f726c01a171c807a8a70cccb8c3c5
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 12:55:02 +0330

    Set properties for municipality

--- WasteManagementAPI/Models/Municipality.cs
+++ WasteManagementAPI/Models/Municipality.cs
@@ -2,5 +2,13 @@
 {
     public class Municipality
     {
+        public int MunicipalityId { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+
+        public string Mayor { get; set; } = string.Empty;
+
+
+
     }
 }
