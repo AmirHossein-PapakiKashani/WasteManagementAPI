3895822277c6661df0531f0aafb5657a16c8cce2
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 14:44:50 +0330

    Create Dbcontext for models

---
+++ WasteManagementAPI/Models/WastMangementContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WasteManagementAPI.Models.DomainModels;
+
+namespace WasteManagementAPI.Models
+{
+    public class WastMangementContext : DbContext
+    {
+        public WastMangementContext(DbContextOptions<WastMangementContext> options) : base(options) { }
+
+        public DbSet<Citizen> Citizens { get; set; }
+
+        public DbSet<Contractor> Contractors { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Municipality> Municipality { get; set;}
+
+        public DbSet<SumOfWeghitOfProducts> SumOfWeghitOfProducts { get; set; }
+
+        public DbSet<Supervisor> Supervisor { get; set; }
+
+        public DbSet<Product> Products { get; set; }    
+
+    }
+}
