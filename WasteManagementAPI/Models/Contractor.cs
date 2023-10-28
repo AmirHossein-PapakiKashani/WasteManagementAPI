f92ff570db695c949863f83c29c7720512317b1d
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-10-28 13:17:28 +0330

    Add contractor again

---
+++ WasteManagementAPI/Models/Contractor.cs
@@ -0,0 +1,11 @@
+﻿namespace WasteManagementAPI.Models
+{
+    public class Contractor
+    {
+          public int ContractorId { get; set; }
+
+        public string Nmae { get; set; } = string.Empty;
+
+        public string Family { get; set; } = string.Empty;
+    }
+}
