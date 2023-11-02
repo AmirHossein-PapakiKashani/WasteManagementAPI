9523f840044aa02ffee87a12b9cad15725ad3b40
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-02 11:33:00 +0330

    Create class for shipment

---
+++ WasteManagementAPI/Models/DomainModels/Shipment.cs
@@ -0,0 +1,15 @@
+﻿namespace WasteManagementAPI.Models.DomainModels
+{
+    public class Shipment
+    {
+        public int ShipmentId { get; set; }
+
+        public string Weight { get; set; }
+
+        public int PointsAllocated { get; set; }
+
+        public bool ApproveBySupervisor { get; set; }
+
+
+    }
+}
