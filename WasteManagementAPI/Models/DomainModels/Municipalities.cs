8ce8dcc5ebf7acadbb56ab59cd62a6c117d9faa3
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-03 13:24:56 +0330

    Create a relationship between municipality and supervisor

--- WasteManagementAPI/Models/DomainModels/Municipalities.cs
+++ WasteManagementAPI/Models/DomainModels/Municipalities.cs
@@ -6,7 +6,11 @@
 
         public string MunicipalityName { get; set; } = string.Empty;
 
-
+        //nav prop
         public ICollection<Contractors> Contractors { get; set; } = new List<Contractors>();
+
+
+        //nav property
+        public ICollection<Supervisors> Supervisors { get; set; } = new List<Supervisors>();
     }
 }
--- WasteManagementAPI/Models/DomainModels/Supervisors.cs
+++ WasteManagementAPI/Models/DomainModels/Supervisors.cs
@@ -1,15 +1,17 @@
-﻿namespace WasteManagementAPI.Models.DomainModels
+﻿using Microsoft.Identity.Client;
+
+namespace WasteManagementAPI.Models.DomainModels
 {
     public class Supervisors
     {
-        public Supervisors() => Municipality = new HashSet<Municipalities>();
-
+        
         public int SupervisorId { get; set; }
 
         public string Name { get; set; } = string.Empty;
 
-
-        public ICollection<Municipalities> Municipality { get; set; }
-
+        //forigen key
+        public int MunicipalityId { get; set; }
+        //nav prop
+        public Municipalities Municipalities { get; set;} = null!;
     }
 }
