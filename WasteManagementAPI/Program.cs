846d8dcd7e17c82fc8de61b746ddb896ed39cb82
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-04 09:45:38 +0330

    create another database

--- WasteManagementAPI/Program.cs
+++ WasteManagementAPI/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<WastMangementContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("WastMangementContext")));
+builder.Services.AddDbContext<WastMangementGptBaseContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("WastMangementGptBaseContext")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
