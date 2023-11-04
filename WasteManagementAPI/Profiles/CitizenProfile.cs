eef56a1a924d09fd47a8f978ce87e5f2a3466bfc
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-04 15:13:49 +0330

    create a profile folder for mapping my objects

---
+++ WasteManagementAPI/Profiles/CitizenProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WasteManagementAPI.Models.AuthModels;
+using WasteManagementAPI.Models.AuthModels.Login;
+using WasteManagementAPI.Models.DomainModels;
+
+namespace WasteManagementAPI.Profiles
+{
+    public class CitizenProfile : Profile
+    {
+        public CitizenProfile() 
+            {
+                CreateMap<Citizens ,UserModel >();
+                CreateMap<Citizens, UserLogin>();
+            }
+    }
+}
