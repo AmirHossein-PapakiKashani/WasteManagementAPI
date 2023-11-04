bf07c6fdd6c8f8dbf799914d821301879c705490
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-04 13:09:47 +0330

    Add   MunicipalitiesModel for regirster

---
+++ WasteManagementAPI/Models/AuthModels/MunicipalitiesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WasteManagementAPI.Models.AuthModels
+{
+    public class MunicipalitiesModel
+    {
+         public string MunicipalityName { get; set; } 
+
+        public string Password {get ; set; } 
+    }
+}
