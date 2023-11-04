091d109d9689feb9d035d3b9391da942a2958aab
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-04 13:05:20 +0330

    Add class for register collection booth

---
+++ WasteManagementAPI/Models/AuthModels/CollectionBoothModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WasteManagementAPI.Models.AuthModels
+{
+    public class CollectionBoothModel
+    {
+        public string EmployeeName { get; set; } 
+
+        public string EmployeeUserName { get; set; } 
+
+        public string EmployeePasswored { get; set; } 
+
+    }
+}
