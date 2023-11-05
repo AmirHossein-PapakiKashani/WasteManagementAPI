58b362deb79a7172b86eb07fefd7d728cb358fbe
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-05 15:40:29 +0330

    Drop WasteType table

---
+++ WasteManagementAPI/Migrations/20231105114218_DeleteWasteTypeTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteWasteTypeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WasteTypes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WasteTypes",
+                columns: table => new
+                {
+                    WasteTypesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShipmentsId = table.Column<int>(type: "int", nullable: false),
+                    ShipmentId = table.Column<int>(type: "int", nullable: false),
+                    WasteName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WasteTypes", x => x.WasteTypesId);
+                    table.ForeignKey(
+                        name: "FK_WasteTypes_Shipments_ShipmentsId",
+                        column: x => x.ShipmentsId,
+                        principalTable: "Shipments",
+                        principalColumn: "ShipmentsId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WasteTypes_ShipmentsId",
+                table: "WasteTypes",
+                column: "ShipmentsId");
+        }
+    }
+}
