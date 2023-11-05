85bb4abc243ddab3a289e75e79f5e308c36df98e
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-05 13:07:04 +0330

    Create a ShipmetnDto for map data to shipment

---
+++ WasteManagementAPI/Models/Dtos/ShipmentDto.cs
@@ -0,0 +1,17 @@
+﻿using WasteManagementAPI.Models.DomainModels;
+
+namespace WasteManagementAPI.Models.Dtos
+{
+    public class ShipmentDto
+    {
+        
+        public int Weight { get; set; } 
+
+        public int PointsAllocated { get; set; }
+
+        public bool ApproveBySupervisor { get; set; }
+       
+       public ICollection<WasteTypes> WasteTypes { get; set; } =  new List<WasteTypes>();
+
+    }
+}
