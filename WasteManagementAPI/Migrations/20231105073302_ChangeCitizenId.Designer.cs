24da4948ab0cf237fc380f4e1c9340d6d6a6a622
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-05 11:17:05 +0330

    change citizen Id name

--- WasteManagementAPI/Migrations/20231105073302_ChangeCitizenId.Designer.cs
+++ WasteManagementAPI/Migrations/20231105073302_ChangeCitizenId.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WasteManagementAPI.Models;
 
@@ -10,9 +11,11 @@ using WasteManagementAPI.Models;
 namespace WasteManagementAPI.Migrations
 {
     [DbContext(typeof(WastMangementGptBaseContext))]
-    partial class WastMangementGptBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20231105073302_ChangeCitizenId")]
+    partial class ChangeCitizenId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WasteManagementAPI/Migrations/20231105073302_ChangeCitizenId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCitizenId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CitizensId",
+                table: "Citizens",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Citizens",
+                newName: "CitizensId");
+        }
+    }
+}
