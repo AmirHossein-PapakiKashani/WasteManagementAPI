e25c8d5bb77b1d99d4ebbde34da9bdae5104d34d
AmirHossein-PapakiKashani <amirhosseinpapaki@gmail.com>
2023-11-09 12:55:23 +0330

    change database

---
+++ WasteManagementAPI/Migrations/20231109085441_ChangeDatabase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Shipments_CitizensId",
+                table: "Shipments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_CitizensId",
+                table: "Shipments",
+                column: "CitizensId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Shipments_CitizensId",
+                table: "Shipments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shipments_CitizensId",
+                table: "Shipments",
+                column: "CitizensId",
+                unique: true);
+        }
+    }
+}
